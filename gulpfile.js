67321d6dfadec9b76b9337365e3805e7aa178ba5
Manikanta Jalla <manikanta.jalla8@gmail.com>
2022-05-17 00:27:49 -0400

    updated

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const { src, dest } = require("gulp");
+const argv = require("yargs").argv;
+let 
+
+
+/**
+ * arguments 
+ * env - enviroment (mandatory)
+ * ff - feature file/service
+ * tag - given tag/tags
+ */
+
+if(argv.ff !== undefined){
+    var featureOath = argv.ff;
+} else {
+    var feat
+}
